db3719fac4638c2e3fe89053cb2a5a91b0b1826d
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-14 11:30:16 +0100

    implementato service Utente

---
+++ src/main/java/it/prova/pokeronline/service/UtenteService.java
@@ -0,0 +1,37 @@
+package it.prova.pokeronline.service;
+
+import java.util.List;
+
+import it.prova.pokeronline.model.Utente;
+
+public interface UtenteService {
+	
+	public List<Utente> listAllUtenti() ;
+
+	public Utente caricaSingoloUtente(Long id);
+	
+	public Utente caricaSingoloUtenteConRuoli(Long id);
+
+	public void aggiorna(Utente utenteInstance);
+
+	public void inserisciNuovo(Utente utenteInstance);
+
+	public void rimuovi(Utente utenteInstance);
+
+	public List<Utente> findByExample(Utente example);
+	
+	public Utente findByUsernameAndPassword(String username, String password);
+	
+	public Utente eseguiAccesso(String username, String password);
+	
+	public void changeUserAbilitation(Long utenteInstanceId);
+	
+	public Utente findByUsername(String username);
+	
+	public void reset(Utente utenteInstance);
+	
+	public void resettaUtente(Utente utenteInstance);
+	
+	public boolean confrontaPassCodificataConDecodificata(String passwordCodificata, String passwordDecodificata);
+
+}
